a5f1fd73fae0d2f71c9494ad30e864242e7b8590
ritikasantara <rits.santara@gmail.com>
2016-09-16 00:37:12 +0530

    completed fixed background scrolling

--- js/my-script.js
+++ js/my-script.js
@@ -1,46 +1,67 @@
-// smooth scrolling effect 
 
-$(document).ready(function(){
 
-  $('nav a').click(function(){
+$(document).ready(function () {
 
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-    && location.hostname == this.hostname){
-
-      var $target = $(this.hash);
-
-      $target = $target.length && $target
-      || $('[name=' + this.hash.slice(1) +']');
-      if ($target.length){
-
-        var targetOffset = $target.offset().top;
-
-        $('html,body').animate({scrollTop: targetOffset}, 500);
-
-        return false;
-
-      }
-
-    }
-
-  });
-
-// search box highlighting on mouseenter  
-
-  $('input').on({
+// search box hover effect
+    $('input').on({
      
-     mouseenter: function(){
+        mouseenter: function(){
+            
+        $(this).css('border','1px solid #fff');
 
-       $(this).css('border','1px solid #fff');
+        },
 
-     },
+        mouseleave: function(){
+            
+        $(this).css('border','1px solid #87cefa');
 
-     mouseleave: function(){
+        }
 
-      $(this).css('border','1px solid #87cefa');
+    });
 
-     }
-
-  });
-
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll effect
+    
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
 });
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('nav ul li a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('nav ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
+
+
+
+
+
+
+
